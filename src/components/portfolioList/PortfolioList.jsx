e596d502ec9ce7fefe9a5cfb56e577629c8ea38d
andreinalbitoru <andrei.nalbitoru@yahoo.com>
2022-02-08 21:35:03 +0200

    first commit

---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,14 @@
+import "./portfolioList.scss";
+
+function PortflioList({ id, title, active, setSelected }) {
+  return (
+    <li
+      className={active ? "portfolioList active" : "portfolioList"}
+      onClick={() => setSelected(id)}
+    >
+      {title}
+    </li>
+  );
+}
+
+export default PortflioList;
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
+function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            getElement.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+40772259424</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>andrei.nalbitoru@yahoo.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;
